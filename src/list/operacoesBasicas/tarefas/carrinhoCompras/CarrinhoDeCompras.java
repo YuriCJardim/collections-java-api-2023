09ab721fff46e9ad6fcc04d096040a278b1e55e0
YuriCJardim <yuricjardim@gmail.com>
2023-09-19 23:40:52 -0300

    feat: adicionando camada de tratamento

--- src/list/operacoesBasicas/tarefas/carrinhoCompras/CarrinhoDeCompras.java
+++ src/list/operacoesBasicas/tarefas/carrinhoCompras/CarrinhoDeCompras.java
@@ -5,64 +5,70 @@ import java.util.List;
 
 public class CarrinhoDeCompras {
 
-    private List<Item> carrinhoDeCompras;
+    private List<Item> itemList;
 
 
     public CarrinhoDeCompras() {
-        this.carrinhoDeCompras = new ArrayList<>();
+        this.itemList = new ArrayList<>();
     }
 
     public void adicionarItem(String nome, double preco, int quantidade) {
-        carrinhoDeCompras.add(new Item(nome, preco, quantidade));
+        itemList.add(new Item(nome, preco, quantidade));
     }
 
     public void removerItem(String nome) {
         List<Item> itemRemover = new ArrayList<>();
+        if (!itemList.isEmpty()){
         for (Item i :
-                carrinhoDeCompras) {
+                itemList) {
             if (i.getNome().equalsIgnoreCase(nome)) {
                 itemRemover.add(i);
             }
-            carrinhoDeCompras.removeAll(itemRemover);
+            itemList.removeAll(itemRemover);
+        }
+    }else {
+            System.out.println("A lista está vazia!");
         }
     }
 
     public Double calcularValorTotal(String nome) {
-        Double valorTotal = 0.0;
-        for (Item i : carrinhoDeCompras) {
-            if (nome.equalsIgnoreCase(i.getNome())) {
-                valorTotal = i.getPreco() * i.getQuantidade();
-                ;
-
+        if (!itemList.isEmpty()) {
+            Double valorTotal = 0.0;
+            for (Item i : itemList) {
+                if (nome.equalsIgnoreCase(i.getNome())) {
+                    valorTotal = i.getPreco() * i.getQuantidade();
+                }
             }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
         }
-        return valorTotal;
     }
 
     public void exibirItens() {
 
-        for (Item item : carrinhoDeCompras) {
+        for (Item item : itemList) {
             System.out.println(item);
         }
     }
 
     public static void main(String[] args) {
 
-        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+        CarrinhoDeCompras itemList = new CarrinhoDeCompras();
 
-        carrinhoDeCompras.adicionarItem("Leite", 2.99, 3);
+        itemList.adicionarItem("Leite", 2.99, 3);
 
-        carrinhoDeCompras.exibirItens();
-        System.out.println(carrinhoDeCompras.calcularValorTotal("Leite"));
+        itemList.exibirItens();
+        System.out.println(itemList.calcularValorTotal("Leite"));
 
-        carrinhoDeCompras.adicionarItem("cafe", 3.99, 1);
-        System.out.println(carrinhoDeCompras.calcularValorTotal("Cafe"));
+        itemList.adicionarItem("cafe", 3.99, 1);
+        System.out.println(itemList.calcularValorTotal("Cafe"));
 
-        carrinhoDeCompras.exibirItens();
+        itemList.exibirItens();
 
-        carrinhoDeCompras.removerItem("Leite");
+        itemList.removerItem("Leite");
 
-        carrinhoDeCompras.exibirItens();
+        itemList.exibirItens();
 
 
     }
